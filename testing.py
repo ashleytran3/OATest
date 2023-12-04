f81935b6eb1f98ec7885f4a3b1022de1aebd3390
ashley tran <ashleytran@ashleys-MacBook-Pro-2.local>
2023-12-04 10:12:57 -0800

    fixed or to and

--- testing.py
+++ testing.py
@@ -1,5 +1,5 @@
 #hihihihihih
 answer = input("Who is the cutest dog in the whole wide world? ")
-while (answer != "Lucky" or answer != "Cookie"):
+while (answer != "Lucky" and answer != "Cookie"):
     answer = input("Try again. Who is the cutest dog in the whole wide world? ")
 print("Correct. Enjoy a day's worth of good luck :3")
