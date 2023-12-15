7d8713e6b9833e9c579ace1facf208096ad34eb5
Omar Abou-Harb <omarabouharb@Omars-MacBook-Air.local>
2023-12-15 11:45:30 -0800

    input now case insensitive

--- testing.py
+++ testing.py
@@ -1,6 +1,7 @@
 #we need to finish the formal proposal!!!
 #yo estoy de acuerdo
 answer = input("Who is the cutest dog in the whole wide world? ")
-while (answer != "Lucky" and answer != "Cookie"):
+answer = answer.lower()
+while (answer != "lucky" and answer != "cookie"):
     answer = input("Try again. Who is the cutest dog in the whole wide world? ")
 print("Correct. Enjoy a day's worth of good luck :3")
